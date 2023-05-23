12facabb801882759b47a9ec30e9a630881820c4
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-05-23 22:03:32 +0300

    setup react-router-dom and MUI

---
+++ src/pages/Settings.js
@@ -0,0 +1,15 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const Settings = () => {
+  return (
+    <div>
+      <Typography variant="h2" fontWeight="bold">
+        Quiz App
+      </Typography>
+      Settings
+    </div>
+  );
+};
+
+export default Settings;
