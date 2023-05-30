9f05a3dc79dbb652bcb8a437338f62c5aa1131f5
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-05-30 12:30:19 +0300

    created finalScreen page

--- src/pages/FinalScreen.js
+++ src/pages/FinalScreen.js
@@ -1,8 +1,25 @@
+import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
+import { handleAmountChange, handleScoreChange } from '../redux/actions';
 
 const FinalScreen = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { score } = useSelector(state => state);
+
+  const handleBackToSettings = () => {
+    dispatch(handleScoreChange(0))
+    dispatch(handleAmountChange(50))
+    navigate('/');
+  }
+
   return (
-    <div>FinalScreen</div>
+    <Box mt={30}>
+      <Typography variant="h3" fontWeight="bold" mb={3}>Final Score {score}</Typography>
+      <Button onClick={handleBackToSettings} variant="outlined">back to settings!</Button>
+    </Box>
   )
 }
 
